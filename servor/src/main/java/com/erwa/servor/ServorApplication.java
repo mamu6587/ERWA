3b07fd91e9176a7fd213e00d592d0e599b67d2bf
mamu6587 <magnus.muhr.6587@student.uu.se>
2020-12-14 17:38:56 +0100

    Defined structure

---
+++ servor/src/main/java/com/erwa/servor/ServorApplication.java
@@ -0,0 +1,13 @@
+package com.erwa.servor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServorApplication.class, args);
+	}
+
+}
